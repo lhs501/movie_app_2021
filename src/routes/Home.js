f842ac2ec43ea8da69b7bdcd99d22d019bba5c89
lhs <lhs@lhsui-MacBookAir.local>
2021-04-05 00:36:19 +0900

    최종 완성 코드

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,78 +1,58 @@
 import React from 'react';
-import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
-import About from './routes/About';
-import Home from './routes/Home';
-import Navigation from './components/Navigation';
-import Detail from './routes/Detail';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
 
-function App() {
-  return(
-    <HashRouter>
-      <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie-detail" component={Detail}/>
-    </HashRouter>
-  );
-}
- 
-export default App;
+class Home extends React.Component {
   
+  state = {
+    isLoading: true,
+    movie: [],
+  };
 
-
-
-
-
-
-//   state = {
-//     isLoading: true,
-//     movie: [],
-//   };
-
-//   getMovies = async () =>{
-//     const{
-//       data: {
-//         data: { movies },
-//       },
-//     } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-//     this.setState({ movies, isLoading: false });
-//   };
-//   componentDidMount(){
-//     //영화 데이터 로딩
-//     this.getMovies();
+  getMovies = async () =>{
+    const{
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount(){
+    //영화 데이터 로딩
+    this.getMovies();
     
-//   }
+  }
   
-//   render(){
-//     const { isLoading, movies } = this.state;
-//     return (
-//     <section className="container">
-//       {isLoading ?(
-//         <div className="loader">
-//           <span className="loader__text">Loading...</span>
-//         </div>
-//       ) :(
-//         <div className="movies">
-//           {movies.map((movie) => (
-//           <Movie
-//             key={movie.id}
-//             id={movie.id}
-//             year={movie.year}
-//             title={movie.title}
-//             summary={movie.summary}
-//             poster={movie.medium_cover_image}
-//             genres={movie.genres}
-//           />
-//         ))}
-//         </div>
-//       )}
-//       </section>
-//     );
-//   }
-// }
+  render(){
+    const { isLoading, movies } = this.state;
+    return (
+    <section className="container">
+      {isLoading ?(
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) :(
+        <div className="movies">
+          {movies.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+            genres={movie.genres}
+          />
+        ))}
+        </div>
+      )}
+      </section>
+    );
+  }
+}
 
-// export default App;
+export default Home;
 
 
 
